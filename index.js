cec1c7d53ed6bdaa195e21c8032fb5a1e6da2e3e
Hassan AbuGareeb <Hassan.AbuGareeb@gmail.com>
2023-09-27 15:51:26 +0300

    task done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names){
+    const thanksCards=[];
+    for(let i=0;i<names.length;i++){
+        thanksCards.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+return thanksCards;
+}
+
+function countDown(number){
+    for(let i=number ; i>=0;i--){
+        console.log(i);
+    }
+
+}
